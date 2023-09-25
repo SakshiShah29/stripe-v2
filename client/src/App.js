7031c1ce729af7dcf54c003d757db011bb37b7ea
Sakshi Shah <sakshi.shah299@gmail.com>
2023-09-26 01:26:31 +0530

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
 import Payment from "./Payment";
 import Completion from "./Completion";
-
+import {Helmet} from "react-helmet"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <main>
+    <Helmet>
+    <meta
+      http-equiv="Content-Security-Policy"
+      content="script-src 'self' https://js.stripe.com; frame-src http: https: http://localhost:4242 https://js.stripe.com https://hooks.stripe.com; img-src * 'self' data: https:;"
+    />
+  </Helmet>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Payment />} />
